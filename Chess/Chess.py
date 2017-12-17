23d2168acf48e94052e04ee27257579330fe5b1c
Ryan <ryan.reid@eagles.cui.edu>
2017-12-16 23:40:05 -0700

    Adding journal from today

--- Chess/Chess.py
+++ Chess/Chess.py
@@ -23,32 +23,46 @@ def vert1(data="  "):
     print("|", data, end="", sep="")
     
 
-def vert2( data2="   "):
+def vert2(data2="   "):
     
     print(data2, "|", end="")
     
     
-def square2(data="  "):
+def square2(number):
     
-    z = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 
+    row()
+    for i in range(0, 8): 
+        vert1(number + i)
+    print("|")
+    print("|", end="")
+    for i in range(0, 8):
+        vert2("N")
+    print()  
+    
+    
+    
+# printing the rows of squares with a start, stop, and step
+for i in range(0, 56, 7):
+    square2(i)
+    
+"""
+ctr = 0
+for i in range(0, 8):
+    while i < 7 and ctr < len(derp):
+      print(derp[ctr])
+      ctr += 1
+      
+      
+derp = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 
 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 
 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 
 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 
 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 
 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 
 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 
-'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8']  
-    row()
-    
-    for i in range(0, 8):
-        vert1(z[i * data])
-    print("|")
-    print("|", end="")
-    for i in range(0, 8):
-        vert2("N")
-    print()    
-    
-
+'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', "ZZZZ", "ZZZ"] 
+        
+"""
 
 
 
@@ -88,8 +102,16 @@ for chessRow in range(1, 9):
     for col in range(1, 9):
         square2(chessRow * col)
  """
-for i in range(1, 9):
-    square2(i)
+ 
+ 
+
+
+
+
+    
+    
+        
+    
     
 row()
 
