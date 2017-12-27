6abbc73e9de9f132717b880b182f6cb8decedc42
Ryan R <ryan.reid@eagles.cui.edu>
2017-12-27 14:00:15 -0700

    Uploading Solution to challenge 1 - Name, age, username

---
+++ DailyProgrammer/1-Name,Age,Username.py
@@ -0,0 +1,18 @@
+"""
+[easy] challenge #1
+submitted 5 years ago by nottoobadguy
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+your name is (blank), you are (blank) years old, and your username is (blank)
+for extra credit, have the program log this information in a file to be accessed late
+
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+your name is (blank), you are (blank) years old, and your username is (blank)
+for extra credit, have the program log this information in a file to be accessed later.
+"""
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+userName = input("What is your user name?: ")
+
+
+print("Your name is", name, ", you are", age, " years old, and your username is ", userName)
