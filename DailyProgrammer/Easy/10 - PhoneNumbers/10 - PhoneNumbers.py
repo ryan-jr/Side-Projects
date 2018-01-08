189f4549d8492cdc10234f02cfdd74585d767fe3
Ryan <ryan.reid@eagles.cui.edu>
2018-01-08 12:57:09 -0700

    updating reddit dailyprogrammer 10 matching US PTNs

---
+++ DailyProgrammer/Easy/10 - PhoneNumbers/10 - PhoneNumbers.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 12:55:20 2018
+
+@author: rjr
+"""
+
+# A mostly completed regex for 
+# https://www.reddit.com/r/dailyprogrammer/comments/pv98f/2182012_challenge_10_easy/
