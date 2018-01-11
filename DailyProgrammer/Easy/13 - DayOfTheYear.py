507065a0e08e4dd085cc3f1cbb5f793166e59c62
Ryan R <ryan.reid@eagles.cui.edu>
2018-01-11 13:49:24 -0700

    Updating to include leap year check

--- DailyProgrammer/Easy/13 - DayOfTheYear.py
+++ DailyProgrammer/Easy/13 - DayOfTheYear.py
@@ -1,12 +1,36 @@
 """
+https://www.reddit.com/r/dailyprogrammer/comments/pzo4w/2212012_challenge_13_easy/
 
 Find the number of the year for the given date. For example, january 1st would be 1, and december 31st is 365.
 
 for extra credit, allow it to calculate leap years, as well.
 
-https://www.reddit.com/r/dailyprogrammer/comments/pzo4w/2212012_challenge_13_easy/
 """
-# Does not currently calculate leap years
+
+def leapCheck(n):
+  """A function to check if a year is a leap year 
+  
+  Args:
+    n: A year provided as an integer
+    
+  Returns:
+    True/False: Returns a boolean, True if a year is a leap year, False otherwise
+    
+  Raises:
+    N/A
+  
+  
+  """
+  
+  if n % 400:
+    return True
+  elif n % 100:
+    return False
+  elif n % 4:
+    return True
+  
+
+
 # Opting to use list due to being able to manipulate them by numeric indicies
 monthList = [ "Jan", 31, 
               "Feb", 28,
@@ -22,15 +46,21 @@ monthList = [ "Jan", 31,
               "Dec", 31 ]
               
 print("A program to determine what day of the year it is")
-userInput = input("Please input the month and day(e.g. Jan, 1)")
+userInput = input("Please input the month and day(e.g. Jan, 1, 2020)")
 
-userMonth, userDay = userInput.split(",")
+
+userMonth, userDay, userYear = userInput.split(",")
 userDay = int(userDay)
+userYear = int(userYear)
+userYear = leapCheck(userYear)
 listIndex = monthList.index(userMonth)
-summation = 0
+
 
 if listIndex == 0:
   print(userDay)
-else:
+elif userYear == False:
   x = monthList[1:listIndex:2]
   print(sum(x) + (monthList[listIndex + 1]) - (monthList[listIndex + 1] - userDay))
+else:
+  x = monthList[1:listIndex:2]
+  print(sum(x) + (monthList[listIndex + 1]) - (monthList[listIndex + 1] - userDay) + 1)
