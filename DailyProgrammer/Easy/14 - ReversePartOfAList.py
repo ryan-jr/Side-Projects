621bf5f3aa6776f44e5c9556c1f892543f49308e
Ryan R <ryan.reid@eagles.cui.edu>
2018-01-14 21:43:05 -0700

    Updating the update

--- DailyProgrammer/Easy/14 - ReversePartOfAList.py
+++ DailyProgrammer/Easy/14 - ReversePartOfAList.py
@@ -1,3 +1,28 @@
+
+"""
+Input: list of elements and a block size k or some other variable of your choice
+
+Output: return the list of elements with every block of k elements reversed, starting from the beginning of the list.
+
+For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the result is 24, 12, 44, 32, 66, 55.
+
+"""
+
+def listSelect(elements, blockSize = 2):
+  newList = []
+  pos = 0
+  while pos < len(elements):
+    newList.extend(reversed(elements[pos:pos + blockSize]))
+    
+    pos += blockSize
+
+  return newList
+
+data = [12, 24, 32, 44, 55, 66]
+print(listSelect(data, 2))
+
+
+
 """
 https://www.reddit.com/r/dailyprogrammer/comments/q2v2k/2232012_challenge_14_easy/
 
@@ -7,6 +32,8 @@ Input: list of elements and a block size k or some other variable of your choice
 Output: return the list of elements with every block of k elements reversed, starting from the beginning of the list.
 
 For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the result is 24, 12, 44, 32, 66, 55.
+
+
 
 """
 
@@ -45,26 +72,4 @@ def listSwitch(userList, blockSize):
 data = [12, 24, 32, 44, 55, 66]
 listSwitch(data, 2)
 
-
-"""
-Input: list of elements and a block size k or some other variable of your choice
-
-Output: return the list of elements with every block of k elements reversed, starting from the beginning of the list.
-
-For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the result is 24, 12, 44, 32, 66, 55.
-
-"""
-
-def listSelect(elements, blockSize = 2):
-  newList = []
-  pos = 0
-  while pos < len(elements):
-    newList.extend(reversed(elements[pos:pos + blockSize]))
-    
-    pos += blockSize
-
-  return newList
-
-data = [12, 24, 32, 44, 55, 66]
-print(listSelect(data, 2))
 
