9cfe2c492992867b1e75f4c36f3b565f6bec19db
Ryan R <ryan.reid@eagles.cui.edu>
2018-01-14 21:41:17 -0700

    Updating solution after finding out about extend

--- DailyProgrammer/Easy/14 - ReversePartOfAList.py
+++ DailyProgrammer/Easy/14 - ReversePartOfAList.py
@@ -44,3 +44,27 @@ def listSwitch(userList, blockSize):
 
 data = [12, 24, 32, 44, 55, 66]
 listSwitch(data, 2)
+
+
+"""
+Input: list of elements and a block size k or some other variable of your choice
+
+Output: return the list of elements with every block of k elements reversed, starting from the beginning of the list.
+
+For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the result is 24, 12, 44, 32, 66, 55.
+
+"""
+
+def listSelect(elements, blockSize = 2):
+  newList = []
+  pos = 0
+  while pos < len(elements):
+    newList.extend(reversed(elements[pos:pos + blockSize]))
+    
+    pos += blockSize
+
+  return newList
+
+data = [12, 24, 32, 44, 55, 66]
+print(listSelect(data, 2))
+
