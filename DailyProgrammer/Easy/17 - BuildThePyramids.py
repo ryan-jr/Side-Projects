524662376f4ddf9f15a6b86bcdd159a542710552
Ryan R <ryan.reid@eagles.cui.edu>
2018-01-24 13:10:46 -0700

    Creating 17 - Build the Pyramids

---
+++ DailyProgrammer/Easy/17 - BuildThePyramids.py
@@ -0,0 +1,21 @@
+
+"""
+
+https://www.reddit.com/r/dailyprogrammer/comments/qheeu/342012_challenge_17_easy/
+
+Note: This challenge text was deleted so I am reconstructing this based off of comments and solutions I looked at (cheating I know)
+"""
+
+
+def triangleBuilder(height = 3):
+  """A function to build a triangle of a given height
+  
+  """
+  
+  for i in range(0, height + 1):
+    s = ""
+    character = "@"
+    s += character * i
+    print(s)
+    
+triangleBuilder()
