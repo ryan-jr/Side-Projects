aeb7fcd0ea29ec1d6bdaf8b6fb4e38fbd5bb6577
Ryan R <ryan.reid@eagles.cui.edu>
2018-01-24 15:00:22 -0700

    Updating challenge 17

--- DailyProgrammer/Easy/17 - BuildThePyramids.py
+++ DailyProgrammer/Easy/17 - BuildThePyramids.py
@@ -1,21 +1,36 @@
-
 """
+
 
 https://www.reddit.com/r/dailyprogrammer/comments/qheeu/342012_challenge_17_easy/
 
 Note: This challenge text was deleted so I am reconstructing this based off of comments and solutions I looked at (cheating I know)
+
+  
+
 """
 
-
 def triangleBuilder(height = 3):
+    """A function to build a triangle of a given height
+  
+  """
+    for i in range(0, height + 1):
+      s = ""
+      character = "@"
+      s += character * i
+      print(s)
+    
+    
+
+def upsideDownTriangleBuilder(height = 3):
   """A function to build a triangle of a given height
   
   """
-  
-  for i in range(0, height + 1):
+    
+  for i in range(height + 1, 0, -1):
     s = ""
     character = "@"
-    s += character * i
-    print(s)
+    space = " "
+    s += character
+    print(character * i)
     
-triangleBuilder()
+upsideDownTriangleBuilder()
