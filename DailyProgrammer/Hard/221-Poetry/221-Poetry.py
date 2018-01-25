5b9c8fa2045c2febec304ca3555deeed41d5f2c9
Ryan <ryan.reid@eagles.cui.edu>
2018-01-25 12:58:25 -0700

    updating the solved version of poetry

--- DailyProgrammer/Hard/221-Poetry/221-Poetry.py
+++ DailyProgrammer/Hard/221-Poetry/221-Poetry.py
@@ -11,31 +11,17 @@ import datetime
 # if they contain the most common words in the dictionary, if they do, add that line 
 # to a list
 
-words = []
-ngrams = ["bk",  "fq",  "jc",  "jt",  "mj",  "qh",  "qx",  "vj",  "wz",  "zh",
-"bq",  "fv",  "jd",  "jv",  "mq",  "qj",  "qy",  "vk",  "xb",  "zj", "bx",  "fx",
-"jf",  "jw",  "mx",  "qk",  "qz",  "vm",  "xg",  "zn", "cb",  "fz",  "jg",  "jx",
-"mz",  "ql",  "sx",  "vn",  "xj",  "zq", "cf", "gq",  "jh",  "jy",  "pq",  "qm",
-"sz",  "vp",  "xk",  "zr", "cg",  "gv",  "jk",  "jz",  "pv",  "qn",  "tq",  "vq",
-"xv",  "zs", "cj",  "gx",  "jl",  "kq",  "px",  "qo",  "tx",  "vt",  "xz",  "zx",
-"cp",  "hk",  "jm",  "kv",  "qb",  "qp",  "vb",  "vw",  "yq", "cv",  "hv",  "jn",
-"kx",  "qc",  "qr",  "vc",  "vx",  "yv", "cw",  "hx",  "jp",  "kz",  "qd",  "qs",
-"vd",  "vz",  "yz", "cx",  "hz",  "jq",  "lq",  "qe",  "qt",  "vf",  "wq",  "zb",
-"dx",  "iy",  "jr",  "lx",  "qf",  "qv",  "vg",  "wv",  "zc", "fk",  "jb",  "js",
-"mg",  "qg",  "qw",  "vh",  "wx",  "zg"]
-
-poetryWords = ["time", "light", "night", "long", "love", "man", "yes", "white", "world", "face", "air", "left", "black", "water", "head", "life", "day", "hand", "people", "wind",
-"inside", "sea" "red", "things", "lost"]
-
 print(datetime.datetime.now())
 
 with open("belowSeventy.txt", "r") as f:
     x = f.readlines()
     
 ctr = 0  
+
+# Creating a list of words from the dictionary
 x = list(map(lambda s: s.strip(), x))
 
-ctrList = []
+# Opening the text file, splitting each line into words and evalutating it.
 with open("poetry.txt", "r") as f:
     for line in f:
         ctr = 0
@@ -46,21 +32,9 @@ with open("poetry.txt", "r") as f:
                 with open("sentences2.txt", "a") as g:
                     if word in x:
                         ctr += 1
-                        ctrList.append(ctr)
                         if ctr >= 4:
-                            print(ctr)
-                            print(datetime.datetime.now())
-                            #words.append(word)
                             g.write(line)
-                            #g.write(">>> ")
-                            #g.write(word)
-                            #g.write("\n")
                             break
-                                
-                                
-                                        
-    
-print(datetime.datetime.now())
 """
 # appending the line if if one of the words from poetry matches 
                 if word in poetryWords:
