492033e037b074e53dca203e2386644b17a7d20e
Ryan <ryan.reid@eagles.cui.edu>
2018-01-25 12:52:18 -0700

    updating the solved version of poetry

---
+++ CNC2018/FindAJob/Assignment1.md
@@ -0,0 +1,127 @@
+A.  Companies I want to work for:
+
+1.  Amazon
+
+A.  500,000+
+
+B.  Seattle/Worldwide (HQ2?)
+
+C.  [Link](https://www.amazon.jobs/location/phoenix-area-az?offset=10&result_limit=10&sort=relevant&distanceType=Mi&radius=24km&latitude=&longitude=&loc_group_id=&loc_query=&base_query=&city=&country=&region=&county=&query_options=&)
+
+D.  Yes
+
+* Meetup
+
+A.  200+ employees
+
+C.  New York
+
+D.  [Link](https://www.meetup.com/jobs/)
+
+E.  Yes
+
+* Penske 
+
+A.  5,000+
+
+B.  Michigan
+
+C. [Link](https://penske.jobs/)
+
+D.  Yes
+
+* Twilio
+
+A.  500+ employees 
+
+B. SF
+
+C. [Link](https://www.twilio.com/company/jobs)
+
+D.  Yes
+
+* Horizon
+
+* CSX
+
+A.  27,000
+
+B.  Jacksonville, FL
+
+C.  [Link](https://csx.taleo.net/careersection/basic_faceted_search/jobsearch.ftl?lang=en)
+
+* UPS
+
+A.  434,000
+
+B.  Atlanta, GA
+
+C.  [Link](https://www.jobs-ups.com/search-jobs/United%20States%20(Country)?orgIds=1187&ac=16451&alp=6252001&alt=2&ascf=[{%22Key%22:%22ALL%22,%22Value%22:null}])
+
+
+* DHL
+
+A.  500,000+
+
+B.  Plantation, FL (USA), Bonn, GER (International)
+
+C.  [Link](https://www.dpdhl.jobs/search-jobs)
+
+
+* Fedex
+
+A.  400,000+
+
+B.  Memphis, TN
+
+C.  [Link](https://careers.fedex.com/fedex/?)[Link](
+
+* Google
+
+A.  75,000+
+
+B.  CA
+
+C.  [Link](https://careers.google.com/jobs)
+
+* Union Pacific
+
+A.  50,000+
+
+B.  Omaha, NE
+
+C.  [Link](https://up.jobs/search-jobs.html)
+
+* BNSF
+
+A.  41,000+
+
+B.  Fort Worth, TX
+
+C.  [Link](https://jobs.bnsf.com/)
+
+* Walmart Labs
+
+A.  3,500+
+
+B.  San Bruno, CA
+
+C.  [Link](walmartlabs.com/jobs)[Link](
+
+* Target
+
+A.  340,000+
+
+B.  Minneapolis, MN
+
+C.  [Link](https://jobs.target.com/)
+
+* Venmo/Paypal
+
+A.  18,000+
+
+B.  San Jose, CA
+
+C.  [Link](https://www.paypal.com/us/webapps/mpp/jobs)
+
+
--- DailyProgrammer/Hard/221-Poetry/221-Poetry.py
+++ DailyProgrammer/Hard/221-Poetry/221-Poetry.py
@@ -4,6 +4,8 @@ Created on Sun Jan 21 22:14:25 2018
 
 @author: rjr
 """
+import datetime
+
 
 # Iterate over the lines of the file checking each of the words and seeing 
 # if they contain the most common words in the dictionary, if they do, add that line 
@@ -25,33 +27,40 @@ ngrams = ["bk",  "fq",  "jc",  "jt",  "mj",  "qh",  "qx",  "vj",  "wz",  "zh",
 poetryWords = ["time", "light", "night", "long", "love", "man", "yes", "white", "world", "face", "air", "left", "black", "water", "head", "life", "day", "hand", "people", "wind",
 "inside", "sea" "red", "things", "lost"]
 
+print(datetime.datetime.now())
+
 with open("belowSeventy.txt", "r") as f:
     x = f.readlines()
     
 ctr = 0  
 x = list(map(lambda s: s.strip(), x))
 
-
+ctrList = []
 with open("poetry.txt", "r") as f:
     for line in f:
-        if len(line) > 100:
-            pass
-        else:
-            for word in line.split():
-                if len(word) >= 15 or len(word) == 1:
-                    pass
-                else:
-                   \
-                    words.append(word)
+        ctr = 0
+        for word in line.split():
+            if len(word) >= 15 or len(word) <= 2:
+                pass
+            else:
+                with open("sentences2.txt", "a") as g:
+                    if word in x:
+                        ctr += 1
+                        ctrList.append(ctr)
+                        if ctr >= 4:
+                            print(ctr)
+                            print(datetime.datetime.now())
+                            #words.append(word)
+                            g.write(line)
+                            #g.write(">>> ")
+                            #g.write(word)
+                            #g.write("\n")
+                            break
+                                
+                                
+                                        
     
-with open("words.txt", "w") as f:
-    for i in words:
-        if i in x:
-            f.write(i)
-            f.write(" ")
-    
-
-
+print(datetime.datetime.now())
 """
 # appending the line if if one of the words from poetry matches 
                 if word in poetryWords:
@@ -60,6 +69,22 @@ with open("words.txt", "w") as f:
 # Trying to add/find words that are the average length in english:
     # (i in x and len(i) >= 3) or 
     
+    words.append(word)
     
+    for word in line.split():
+with open("sentences.txt", "w") as f:
+    for i in words:
+        if i in x:
+            f.write(i)
+            f.write(" ")
+    
+
+print(len(words))
+print(words)
+
+with open("allthewords.txt", "a") as h:
+    for i in words:
+        h.write(" ")
+        h.write(i)
     
 """
