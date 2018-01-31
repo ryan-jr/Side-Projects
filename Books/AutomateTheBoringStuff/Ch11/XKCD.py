ac71c2f55bf3806fe6acc75b732fbb252e7baa0e
Ryan <ryan.reid@eagles.cui.edu>
2018-01-31 00:17:48 -0700

    updating XKCD

--- Books/AutomateTheBoringStuff/Ch11/XKCD.py
+++ Books/AutomateTheBoringStuff/Ch11/XKCD.py
@@ -20,29 +20,29 @@ while not url.endswith("#"):
     res.raise_for_status()
     
     soup = bs4.BeautifulSoup(res.text)
+    
+    # Find the URL of the comic
+    comicElem = soup.select("#comic img")
+    if comicElem == []:
+        print("Could not find an image")
+    else:
+        comicUrl = "http:" + comicElem[0].get("src")
+        # Download the image
+        print("Downloading image %s..." % (comicUrl))
+        res = requests.get(comicUrl)
+        res.raise_for_status()
+        # Save the image to the folder
+        
+        imageFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
+    
+    # Get the Prev button's URL
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = "http://xkcd.com" + prevLink.get("href")
 
 
-# Find the URL of the comic
 
-comicElem = soup.select("#comic img")
-if comicElem == []:
-    print("Could not find an image")
-else:
-    comicUrl = "http:" + comicElem[0].get("src")
-    # Download the image
-    print("Downloading image %s..." % (comicUrl))
-    res = requests.get(comicUrl)
-    res.raise_for_status()
-# Save the image to the folder
-
-imageFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
-for chunk in res.iter_content(100000):
-    imageFile.write(chunk)
-imageFile.close()
-
-
-# Get the Prev button's URL
-prevLink = soup.select('a[rel = "prev"]')[0]
-url = "http://xkcd.com" + prevLink.get("href")
 
 print("done")
