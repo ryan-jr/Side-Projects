8b53e35c3ecac8019c4b641c5965911183797ca1
Ryan <ryan.reid@eagles.cui.edu>
2018-04-29 23:40:05 -0700

    Adding journal from today

--- FCC/FrontEndDevCert/advancedProjects/calculator/calculator.js
+++ FCC/FrontEndDevCert/advancedProjects/calculator/calculator.js
@@ -1,12 +1,90 @@
 // JS code here
 
+var calculationHolder = []
 
-function showDiv() {
+function showDiv(clickedId) {
 	document.getElementById("window").style.display="";
-	console.log("Hello");
+
+
+	// https://stackoverflow.com/questions/4825295/javascript-onclick-to-get-the-id-of-the-clicked-button
+	console.log(document.getElementById("one"));
+	console.log(document.getElementById("two"));
+
+	console.log(clickedId)
+
+
+}
+
+
+	/* 
+	if (document.getElementById("one") == true) {
+		console.log("one!");
+		var newInput = 1;
+		calculationHolder.push(newInput);
+	} else if (document.getElementById("two") == true) {
+		console.log("two");
+		var input = 2;
+		calculationHolder.push(input);
+
+	} 
+
+	else if (input === "three") {
+		console.log("three");
+		input = 3;
+		calculationHolder.push(input);
+
+	} else if (document.getElementById("four")) {
+		console.log("four");
+		input = 4;
+		calculationHolder.push(input);
+	} else if (document.getElementById("five")) {
+		console.log("five");
+		input = 5;
+		calculationHolder.push(input);
+	} else if (document.getElementById("six")) {
+		console.log("six");
+		input = six;
+		calculationHolder.push(input);
+	} else if (document.getElementById("seven")) {
+		console.log("seven");
+		input = 7;
+		calculationHolder.push(input);
+	} else if (document.getElementById("eight")) {
+		console.log("eight");
+		input = 8;
+		calculationHolder.push(input);
+	} else if (document.getElementById("nine")) {
+		console.log("nine");
+		input = 9;
+		calculationHolder.push(input);
+	} else if (document.getElementById("zero")) {
+		console.log("zero");
+		input = 0;
+		calculationHolder.push(input);
+	}
+
+
+
+*/
+	
+	//console.log(calculationHolder);
+
+/*
+	for(i = 0; i < calculationHolder.length; i++) {
+		console.log("Hello");
+		document.getElementById("window").innerHTML = calculationHolder;
+		
+	}
+*/
+
+
+
+
+function total() {
+	// Function to total everything in the calculationHolder array
+	// Stub function for now
+
 }
 
 
 
-/* When the HTML button is pressed
-That number is strinified
